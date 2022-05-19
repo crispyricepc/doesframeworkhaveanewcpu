4b8bca8776cdcb0af373ee3201a992f38fac9e4c
Ben Mitchell <bjosephmitchell@gmail.com>
2022-05-19 14:03:52 +0100

    He exclaimed,

--- script.js
+++ script.js
@@ -12,5 +12,5 @@ answers = [
 
 window.onload = function () {
   document.querySelector("#question").innerHTML = questions[Math.floor(Math.random() * questions.length)];
-  document.querySelector("#answer").innerHTML = `${answers[Math.floor(Math.random() * answers.length)]}.`;
+  document.querySelector("#answer").innerHTML = `${answers[Math.floor(Math.random() * answers.length)]}!`;
 };
